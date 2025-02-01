a250b76eb9a10d0982221e1451ddff1f3feb5330
Asif-Moon <asifaslam.ch94@gmail.com>
2025-02-01 23:50:20 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://oroton.com/clara-mini-bag-gold/";
+    const whitePageURL = "https://olgaberg.com/collections/olga-berg-icons/products/estelle-feather-clutch-black";
     const blackPageURL = "https://adffuaorrd.myfunnelish.com/imbassd-1736979042571105-1737989463763779";
   
     // Parse the UTM parameters from the request URL
